dc1e9423c9796b9f8ce20f2e0748b6b691dd8196
Sara Delcheva <saradelcheva@gmail.com>
2025-05-19 15:20:14 +0200

    no

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -23,8 +23,17 @@ if (process.env.NODE_ENV === "production" && !process.env.MONGODB_URI) {
   throw new Error("Please add your MongoDB URI to .env.local");
 }
 
-const uri = process.env.MONGODB_URI || "";
-const options = {};
+const uri = process.env.MONGODB_URI as string;
+const options = {
+  maxPoolSize: 10,
+  minPoolSize: 5,
+  maxIdleTimeMS: 10000,
+  connectTimeoutMS: 5000,
+  socketTimeoutMS: 5000,
+  serverSelectionTimeoutMS: 5000,
+  retryWrites: true,
+  retryReads: true,
+};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
